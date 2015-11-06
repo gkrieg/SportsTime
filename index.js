4b607f2154b0595c4f2e73bfc4333973c236d1c8
Spencer Krieger <gkrieg13@gmail.com>
2015-11-06 11:43:40 -0700

    helloworld

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ var app = express();
 app.set('port',(process.env.PORT || 5500));
 app.use(express.static(__dirname + '/public'));
 
-console.log("Hello World");
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(5500);
