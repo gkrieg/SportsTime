32cf64507bd64c742f2e69c78aee32beda7bb5af
samwarnick <sam.t.warnick@gmail.com>
2015-11-06 23:48:28 -0700

    Add SportCtrl

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,54 +1,45 @@
 
 SportsTimeApp.controller('UserCtrl', ['$scope','$http','UserService',function ($scope, $http,UserService) {
 
-
     $scope.user = UserService;
-$scope.insertUser = function(data){
 
+    $scope.save = function() {
 
-    alert("something");
+        var str = { "firstname": $scope.user.firstname,
+        "lastname": $scope.user.lastname,
+        "age": $scope.user.age,
+        "email": $scope.user.email,
+        "phone": $scope.user.phone,
+        "zip": $scope.user.zip,
+        "password": $scope.user.password}
+        ;
+        alert(JSON.stringify(str))
+        $http({
+          method: 'POST',
+          url: '/sportstime/insertUser',
+          data: JSON.stringify(str)
+
+        }).then(function successCallback(response) {
+            $scope.sports = data;
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        })
+        $scope.goToState('home');
+
+    };
+}]);
+
+SportsTimeApp.controller('SportsCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.categories = [];
+
     $http({
-      method: 'POST',
-      url: '/sportstime/insertUser/'
+        method: 'GET',
+        url: '/sportstime/getSportsCategories'
     }).then(function successCallback(response) {
-        $scope.sports = data;
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-}
-
-$scope.save = function() {
-
-
-    var str = { "firstname": $scope.user.firstname,
-    "lastname": $scope.user.lastname,
-    "age": $scope.user.age,
-    "email": $scope.user.email,
-    "phone": $scope.user.phone,
-    "zip": $scope.user.zip,
-    "password": $scope.user.password}
-    ;
-    alert(JSON.stringify(str))
-    $http({
-      method: 'POST',
-      url: '/SportsTime/insertUser',
-      data: JSON.stringify(str)
-
-    }).then(function successCallback(response) {
-        $scope.sports = data;
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
+        $scope.categories = response.data;
+    }), function errorCallback(response) {
+        return;
     })
-    $scope.goToState('home');
-
-};
-
-
-
-
-
-
-
 }]);
